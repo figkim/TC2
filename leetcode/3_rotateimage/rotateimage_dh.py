a0f2e4831277813603a3a007795ccd7cacfb5b92
Data Analytics CoE 김도형 <fig.kim@sk.com>
2019-07-03 11:52:24 +0900

    add simple algorithm description.

--- leetcode/3_rotateimage/rotateimage_dh.py
+++ leetcode/3_rotateimage/rotateimage_dh.py
@@ -2,8 +2,21 @@ import copy
 class Solution(object):
     def rotate(self, matrix):
         """
-        :type matrix: List[List[int]]
-        :rtype: None Do not return anything, modify matrix in-place instead.
+        in list-memory perspective, rotate function just re-arrange
+        the order of memory. Refer to the symbols allocated to each
+        number. You can easily find the pattern `rotate --> matrix[j][n - i - 1]`
+
+        matrix      -->     rotated matrix
+        [0] |1|*                [0] |7|@
+            |2|*                    |4|$
+            |3|*                    |1|*
+        [1] |4|$                [1] |8|@
+            |5|$                    |5|$
+            |6|$                    |2|*
+        [2] |7|@                [2] |9|@
+            |8|@                    |6|$
+            |9|@                    |3|*
+
         """
         n = len(matrix)
         for i, mem in enumerate(copy.deepcopy(matrix)):
