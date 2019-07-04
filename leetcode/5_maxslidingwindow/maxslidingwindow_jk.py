e50680466b8f6dbce702c0c4a1f1077fe46418da
junkeon <35945268+junkeon@users.noreply.github.com>
2019-07-04 21:54:34 +0900

    Update maxslidingwindow_jk.py
    time : 124 ms / space : 20.1 mb

--- leetcode/5_maxslidingwindow/maxslidingwindow_jk.py
+++ leetcode/5_maxslidingwindow/maxslidingwindow_jk.py
@@ -1,7 +1,21 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         N = len(nums)
-        if k > 0:
-            return [max(nums[i:i+k]) for i in range(N - k + 1)]
-        else:
+        if N == 0:
             return []
+        if k == 1:
+            return nums
+
+        max_num = max(nums[:k])
+        max_list = [max_num]
+
+        for i in range(k, N):
+            if max_num <= nums[i]:
+                max_num = nums[i]
+
+            elif max_num == nums[i-k]:
+                max_num = max(nums[i-k+1:i+1])
+
+            max_list.append(max_num)
+
+        return max_list
