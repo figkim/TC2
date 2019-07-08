8b295cb3e0f396c731557a67c5c95f5975487bfd
junkeon <35945268+junkeon@users.noreply.github.com>
2019-07-08 18:24:50 +0900

    Update generateParenthesis_jk.py
    remove valid function
    time : 52ms / space : 13.4mb

--- leetcode/7_Generate Parentheses/generateParenthesis_jk.py
+++ leetcode/7_Generate Parentheses/generateParenthesis_jk.py
@@ -1,32 +1,21 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        def valid(token):
-            cnt = 0
+        stack = [('(', 1)]
 
-            for sym in token:
-                if sym == '(':
-                    cnt += 1
-                else:
-                    cnt -= 1
-
-                if cnt < 0:
-                    return False
-
-            return cnt == 0
-
-        stack = ['(']
         ans = []
 
         while len(stack) > 0:
-            token = stack.pop()
-            
-            if len(token) == 2 * n:
-                if valid(token):
-                    ans.append(token)
-                    
+            token, score = stack.pop()
+
+            if score < 0:
                 continue
 
-            stack.append(token + '(')
-            stack.append(token + ')')        
+            if len(token) == 2 * n:
+                if score == 0:
+                    ans.append(token)
+                continue
+
+            stack.append((token + '(', score + 1))
+            stack.append((token + ')', score - 1))      
                 
         return ans
