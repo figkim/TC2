64a445cc5813f7ec6bbcdf325bf6d444896b2746
gtshs2 <kyungwoo.song@gmail.com>
2019-07-22 09:47:23 +0900

    perm

---
+++ leetcode/015_nextpermutation/nextpermutation.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
