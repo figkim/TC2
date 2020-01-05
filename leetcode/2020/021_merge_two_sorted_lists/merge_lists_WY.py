3d8da0590a68e6230612c2363e1f2cc548d993cc
weonyoung <weonyoungjoo@gmail.com>
2020-01-05 14:14:57 +0900

    add 021

---
+++ leetcode/2020/021_merge_two_sorted_lists/merge_lists_WY.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        
+        new_list = ListNode(None)
+        current = new_list
+        
+        while l1 and l2:
+            if l1.val < l2.val:
+                current.next = l1
+                l1 = l1.next
+            else:
+                current.next = l2
+                l2 = l2.next
+            current = current.next
+        if l1:
+            current.next = l1
+        elif l2:
+            current.next = l2
+        
+        return new_list.next
