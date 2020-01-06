d5935a3cd3be9e3cbe821d76774fba051cba83ff
doyun kim <maybedy@users.noreply.github.com>
2020-01-06 22:43:03 +0900

    Create strStr_dy.py

---
+++ leetcode/2020/028_implement_strStr/strStr_dy.py
@@ -0,0 +1,9 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle == "":
+            return 0
+        if needle in haystack:
+            for i in range(len(haystack)):
+                if haystack[i:i+len(needle)] == needle:
+                    return i
+        return -1
