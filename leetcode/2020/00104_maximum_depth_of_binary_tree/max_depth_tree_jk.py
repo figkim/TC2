b94bfa31e9ce9f7ca9c45574a23bd5ce7e4f63a5
junkeon <35945268+junkeon@users.noreply.github.com>
2020-01-12 23:59:46 +0900

    Update max_depth_tree_jk.py
    Using the shorter and cleaner one with recursive

--- leetcode/2020/00104_maximum_depth_of_binary_tree/max_depth_tree_jk.py
+++ leetcode/2020/00104_maximum_depth_of_binary_tree/max_depth_tree_jk.py
@@ -7,25 +7,4 @@
 
 class Solution:
     def maxDepth(self, root: TreeNode) -> int:
-        
-        if not root:
-            return 0
-        
-        if not (root.left or root.right):
-            return 1
-        
-        queue = [(root, 1)]        
-        max_depth = 1
-        
-        while queue:
-            node, depth = queue.pop()
-            
-            if node.left:
-                queue.append((node.left, depth+1))
-                max_depth = max(max_depth, depth+1)
-                
-            if node.right:
-                queue.append((node.right, depth+1))
-                max_depth = max(max_depth, depth+1)
-                
-        return max_depth
+        return max(self.maxDepth(root.left), self.maxDepth(root.right))+1 if root else 0
