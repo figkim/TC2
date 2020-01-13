39bd6306df8f9eab7407c3d324070d429e2528c3
junkeon <35945268+junkeon@users.noreply.github.com>
2020-01-13 22:07:59 +0900

    Create path_sum_jk.py
    Runtime: 40 ms, faster than 77.46% of Python3 online submissions for Path Sum.
    Memory Usage: 14.4 MB, less than 100.00% of Python3 online submissions for Path Sum.

---
+++ leetcode/2020/00112_path_sum/path_sum_jk.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        if not root:
+            return False
+        
+        if not (root.left or root.right):
+            return root.val == sum
+        
+        return self.hasPathSum(root.left, sum-root.val) or self.hasPathSum(root.right, sum-root.val)
