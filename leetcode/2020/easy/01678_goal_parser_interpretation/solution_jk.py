92829e63d85440721f4232f09a0246efeaf3e4c8
junkeon <35945268+junkeon@users.noreply.github.com>
2020-12-07 14:50:56 +0900

    Create solution_jk.py
    Runtime: 36 ms, faster than 25.00% of Python3 online submissions for Goal Parser Interpretation.
    Memory Usage: 14.3 MB, less than 25.00% of Python3 online submissions for Goal Parser Interpretation.

---
+++ leetcode/2020/easy/01678_goal_parser_interpretation/solution_jk.py
@@ -0,0 +1,7 @@
+class Solution:
+    def interpret(self, command: str) -> str:
+        
+        command = command.replace('()', 'o')
+        command = command.replace('(al)', 'al')
+        
+        return command
