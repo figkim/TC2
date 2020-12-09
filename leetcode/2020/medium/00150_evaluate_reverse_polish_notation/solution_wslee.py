959003f19756aee2969b01c15d5fb7f530ca98ea
AD-Tech개발팀 이원성 <wonsung.lee@sk.com>
2020-12-09 20:36:19 +0900

    RPN solution_wslee added (runtime: 24.93%, memory: 98.24%)

--- leetcode/2020/medium/00150_evaluate_reverse_polish_notation/solution_wslee.py
+++ leetcode/2020/medium/00150_evaluate_reverse_polish_notation/solution_wslee.py
@@ -6,7 +6,7 @@ class Solution:
         #         print(tokens[0])
         idx = 1
         while True:
-            if len(operand_stack) == 0 or idx == len(tokens):
+            if idx == len(tokens):
                 break
             token = tokens[idx]
             if token in operators:
@@ -23,34 +23,16 @@ class Solution:
         return operand_stack[0]
 
     def compute(self, operand_stack, operator):
+        second_operand = int(operand_stack.pop())
+        first_operand = int(operand_stack.pop())
 
-        if len(operand_stack) == 1:
-            return str(operand_stack.pop())
-        else:
-            second_operand = int(operand_stack.pop())
-            first_operand = int(operand_stack.pop())
-
-            if operator == '+':
-                result = first_operand + second_operand
-            elif operator == '-':
-                result = first_operand - second_operand
-            elif operator == '*':
-                result = int(first_operand * second_operand)
-            elif operator == '/':
-                result = int(first_operand / second_operand)
+        if operator == '+':
+            result = first_operand + second_operand
+        elif operator == '-':
+            result = first_operand - second_operand
+        elif operator == '*':
+            result = int(first_operand * second_operand)
+        elif operator == '/':
+            result = int(first_operand / second_operand)
 
         return result
-
-# more simpler solution... 현타오네
-# class Solution:
-#     def evalRPN(self, tokens: List[str]) -> int:
-#         res = []
-#         ops = ['+','-','*', '/']
-#         for token in tokens:
-#             if token in ops:
-#                 n2 = res.pop() # Pop out in reverse order
-#                 n1 = res.pop()
-#                 res.append(int(eval(str(n1) + token + str(n2)))) # evaluate statement
-#             else:
-#                 res.append(token) # keep on appending numbers
-#         return res[0]
