b392f90fb689a63ac85bdbf870a081b9ce1d42df
weonyoung <weonyoungjoo@gmail.com>
2020-12-14 23:11:54 +0900

    GG

---
+++ leetcode/2020/medium/00039_combination_sum/solution_WY.py
@@ -0,0 +1,21 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+
+        results = []
+        def backtrack(remain, comb, start):
+            if remain == 0:
+                # make a deep copy of the current combination
+                results.append(list(comb))
+                return
+            elif remain < 0:
+                # exceed the scope, stop exploration.
+                return
+            for i in range(start, len(candidates)):
+                # add the number into the combination
+                comb.append(candidates[i])
+                # give the current number another chance, rather than moving on
+                backtrack(remain - candidates[i], comb, i)
+                # backtrack, remove the number from the combination
+                comb.pop()
+        backtrack(target, [], 0)
+        return results
