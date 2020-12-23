36b2a6e087ab0cb7d764faa17a04a26bb42ca9a0
junkeon <35945268+junkeon@users.noreply.github.com>
2020-12-23 18:05:00 +0900

    Create solution_jk.py
    Runtime: 1672 ms, faster than 5.03% of Python3 online submissions for Sudoku Solver.
    Memory Usage: 14.5 MB, less than 15.63% of Python3 online submissions for Sudoku Solver.

---
+++ leetcode/2020/hard/00037_sudoku_solver/solution_jk.py
@@ -0,0 +1,52 @@
+import copy
+
+def vertical_check(i, n, board):
+    for j in range(9):
+        if board[i][j] != '.' and n == int(board[i][j]):
+            return False
+    return True
+
+def horizontal_check(j, n, board):
+    for i in range(9):
+        if board[i][j] != '.' and n == int(board[i][j]):
+            return False
+    return True
+
+def zone_check(k, n, board):
+    x, y = divmod(k, 3)
+    x *= 3
+    y *= 3
+    for i in range(3):
+        for j in range(3):
+            if board[x+i][y+j] != '.' and n == int(board[x+i][y+j]):
+                return False
+    return True
+
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        _board = copy.deepcopy(board)
+        queue = [(0, _board)]
+        
+        zeros = [(i, j) for i in range(9) for j in range(9) if board[i][j] == '.']
+
+        while queue:
+            index, _board = queue.pop()
+
+            if index == len(zeros):
+                print(_board)
+                break
+
+            for num in range(1, 10):
+                i, j = zeros[index]
+                k = 3*(i//3) + (j//3)
+                if vertical_check(i, num, _board) and horizontal_check(j, num, _board) and zone_check(k, num, _board):
+                    __board = copy.deepcopy(_board)
+                    __board[i][j] = str(num)
+                    queue.append((index+1, __board))
+
+        for i in range(9):
+            for j in range(9):
+                board[i][j] = _board[i][j]
