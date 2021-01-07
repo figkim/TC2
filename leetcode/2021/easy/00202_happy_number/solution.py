28c3901e14282d32f5c120c562670fdaa5a1f205
aporia3517 <muzekaline@gmail.com>
2021-01-07 11:04:16 +0900

    time: 36ms (56.69%), memory: 14.4MB (15.39%)

--- leetcode/2021/easy/00202_happy_number/solution.py
+++ leetcode/2021/easy/00202_happy_number/solution.py
@@ -1,2 +1,18 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
+
+        num_set = set()
+        while n != 0 and n not in num_set:
+            num_set.add(n)
+            tmp_sum = 0
+            str_n = str(n)
+            for i in range(len(str_n)):
+                tmp_sum += int(str_n[i]) ** 2
+            n = tmp_sum
+
+            print(n)
+
+        if n == 1:
+            return True
+        return False
+
